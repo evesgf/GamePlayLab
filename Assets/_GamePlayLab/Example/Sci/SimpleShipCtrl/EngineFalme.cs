e4375639b619edc11b2745663c489208f1e07902
evesgf <evesgf@163.com>
2019-06-09 03:46:17 +0800

    添加爆炸粒子

---
+++ Assets/_GamePlayLab/Example/Sci/SimpleShipCtrl/EngineFalme.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineFalme : MonoBehaviour
+{
+    public string inputAxis = "Vertical";
+
+    public Vector3 minFalmeScale;
+    public Vector3 maxFalmeScale;
+    public Transform falme;
+
+    public float minStar=0;
+    public float maxStar=20;
+    public ParticleSystem star;
+
+    private ParticleSystem.EmissionModule emission;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        falme.localScale = minFalmeScale;
+        emission=star.emission;
+        emission.rateOverTime = minStar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        falme.localScale = Vector3.Lerp(minFalmeScale, maxFalmeScale, Mathf.Clamp01(Input.GetAxis(inputAxis)));
+        emission.rateOverTime = maxStar * Mathf.Clamp01(Input.GetAxis(inputAxis));
+    }
+}
