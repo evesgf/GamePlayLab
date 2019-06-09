e0f7be1f3d6f2df50b55a8390dde51ed8605aa37
evesgf <evesgf@163.com>
2019-06-09 11:52:38 +0800

    修复跳跃速度缩放控制

--- Assets/_GamePlayLab/Example/Sci/Warp_Jump/WarpJumpEffect.cs
+++ Assets/_GamePlayLab/Example/Sci/Warp_Jump/WarpJumpEffect.cs
@@ -2,104 +2,114 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WarpJumpEffect : MonoBehaviour
+namespace GPL
 {
-    public bool isIn = false;
-    public ParticleSystem warpRings;
-    public Vector3 warpRangeScale=new Vector3(1,1,1.2f);
-    public ParticleSystem warpMoveing;
-    public Transform ShipPos;
-    public float ShipJumpSpeed;
-    public Vector3 ShipJumpStartPoint;
-    public Vector3 ShipJumpEndPoint;
-    public bool isPlayAwake = false;
 
-    bool isRings;
-    bool isWarping;
-
-    private ParticleSystem[] particles;
-
-    IEnumerator OnStartWarp(Transform avatar, float speed = 1f)
+    public class WarpJumpEffect : MonoBehaviour
     {
-        BroadcastMessage("OnSpawned", SendMessageOptions.DontRequireReceiver);
-        ShipPos = avatar;
-        isRings = false;
-        isWarping = false;
-        warpMoveing.transform.localPosition = ShipJumpStartPoint;
-        ShipPos.position = warpMoveing.transform.position;
+        public bool isIn = false;
+        public ParticleSystem warpRings;
+        public Vector3 warpRangeScale = new Vector3(1, 1, 1.2f);
+        public ParticleSystem warpMoveing;
+        public Transform ShipPos;
+        public float ShipJumpSpeed;
+        public Vector3 ShipJumpStartPoint;
+        public Vector3 ShipJumpEndPoint;
+        public bool isPlayAwake = false;
 
-        particles = GetComponentsInChildren<ParticleSystem>();
-        foreach (var p in particles)
+        bool isRings;
+        bool isWarping;
+
+        private ParticleSystem[] particles;
+
+        IEnumerator OnStartWarp(Transform avatar, float speed = 1f)
         {
-            var sp = p.main.simulationSpeed;
-            sp = speed;
+            BroadcastMessage("OnSpawned", SendMessageOptions.DontRequireReceiver);
+            ShipPos = avatar;
+            isRings = false;
+            isWarping = false;
+            warpMoveing.transform.localPosition = ShipJumpStartPoint;
+            ShipPos.position = warpMoveing.transform.position;
+
+            particles = GetComponentsInChildren<ParticleSystem>();
+            for (int i = 0; i < particles.Length; i++)
+            {
+                particles[i].playbackSpeed = speed;
+            }
+
+            var tunnels = GetComponentsInChildren<WarpJumpTunnel>();
+            foreach (var tunnel in tunnels)
+            {
+                tunnel.Init(speed);
+            }
+
+            yield return new WaitForSeconds(warpRings.main.startDelayMultiplier / speed);
+
+            OnRingsScale();
+
+            var t1 = warpMoveing.main.startDelayMultiplier - warpRings.main.startDelayMultiplier;
+
+            yield return new WaitForSeconds(t1 / speed);
+            isRings = false;
+            OnWarpScale();
         }
 
-        yield return new WaitForSeconds(warpRings.main.startDelayMultiplier);
+        private void Start()
+        {
+            if (isPlayAwake)
+                Init(ShipPos);
+        }
 
-        OnRingsScale();
+        // Update is called once per frame
+        void Update()
+        {
+            if (isRings && !isIn)
+                ShipRingsScale();
 
-        var t1 = warpMoveing.main.startDelayMultiplier - warpRings.main.startDelayMultiplier;
+            if (isWarping)
+                ShiftShipPosition();
+        }
 
-        yield return new WaitForSeconds(t1);
-        isRings = false;
-        OnWarpScale();
-    }
+        public void Init(Transform avatar, float speed = 1f)
+        {
+            StartCoroutine(OnStartWarp(avatar, speed));
+        }
 
-    private void Start()
-    {
-        if (isPlayAwake)
-            Init(ShipPos);
-    }
+        public void OnRingsScale()
+        {
+            isRings = true;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isRings && !isIn)
-            ShipRingsScale();
+        public void OnWarpScale()
+        {
+            isWarping = true;
+        }
 
-        if (isWarping)
-            ShiftShipPosition();
-    }
-
-    public void Init(Transform avatar,float speed=1f)
-    {
-        StartCoroutine(OnStartWarp(avatar, speed));
-    }
-
-    public void OnRingsScale()
-    {
-        isRings = true;
-    }
-
-    public void OnWarpScale()
-    {
-        isWarping = true;
-    }
-
-    void ShipRingsScale()
-    {
-        warpMoveing.transform.localScale = Vector3.Lerp(warpMoveing.transform.localScale, warpRangeScale,
-Time.deltaTime * ShipJumpSpeed);
-        ShipPos.localScale = warpMoveing.transform.localScale;
-    }
-
-    void ShiftShipPosition()
-    {
-        warpMoveing.transform.localPosition = Vector3.Lerp(warpMoveing.transform.localPosition, ShipJumpEndPoint,
-            Time.deltaTime * ShipJumpSpeed);
-        ShipPos.position = warpMoveing.transform.position;
-
-        if (isIn)
+        void ShipRingsScale()
         {
             warpMoveing.transform.localScale = Vector3.Lerp(warpMoveing.transform.localScale, warpRangeScale,
-Time.deltaTime * ShipJumpSpeed);
+    Time.deltaTime * ShipJumpSpeed);
+            ShipPos.localScale = warpMoveing.transform.localScale;
         }
-        else
+
+        void ShiftShipPosition()
         {
-            warpMoveing.transform.localScale = Vector3.Lerp(warpMoveing.transform.localScale, Vector3.zero,
-Time.deltaTime * ShipJumpSpeed);
+            warpMoveing.transform.localPosition = Vector3.Lerp(warpMoveing.transform.localPosition, ShipJumpEndPoint,
+                Time.deltaTime * ShipJumpSpeed);
+            ShipPos.position = warpMoveing.transform.position;
+
+            if (isIn)
+            {
+                warpMoveing.transform.localScale = Vector3.Lerp(warpMoveing.transform.localScale, warpRangeScale,
+    Time.deltaTime * ShipJumpSpeed);
+            }
+            else
+            {
+                warpMoveing.transform.localScale = Vector3.Lerp(warpMoveing.transform.localScale, Vector3.zero,
+    Time.deltaTime * ShipJumpSpeed);
+            }
+            ShipPos.localScale = warpMoveing.transform.localScale;
         }
-        ShipPos.localScale = warpMoveing.transform.localScale;
     }
+
 }
