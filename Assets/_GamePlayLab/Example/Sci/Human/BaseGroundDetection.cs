6e589240b485c8b4c7b1484de6845ca55456997e
evesgf <evesgf@163.com>
2019-08-02 14:55:30 +0800

    添加角色控制相关

--- Assets/_GamePlayLab/Example/Sci/Human/BaseGroundDetection.cs
+++ Assets/_GamePlayLab/Example/Sci/Human/BaseGroundDetection.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseGroundDection : MonoBehaviour
+public class BaseGroundDetection : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
