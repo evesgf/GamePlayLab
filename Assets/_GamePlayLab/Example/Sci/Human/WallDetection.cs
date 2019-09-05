65330245290d9155135fdf2bff6ed41a9d08dd7e
evesgf <evesgf@163.com>
2019-09-05 17:33:19 +0800

    添加翻越障碍射线检测

--- Assets/_GamePlayLab/Example/Sci/Human/WallDetection.cs
+++ Assets/_GamePlayLab/Example/Sci/Human/WallDetection.cs
@@ -6,7 +6,8 @@ namespace GPL
 {
     public class WallDetection : MonoBehaviour
     {
-        public LayerMask layerMask;
+        public LayerMask obstacleLayerMask;
+        public LayerMask groundLayerMask;
 
         public Transform forwardUpCheckRoot;
         public float forwardUpCheckDistance = 1.0f;
@@ -23,6 +24,13 @@ namespace GPL
         public bool forwardDownIsHit;
         private RaycastHit forwardDownHit;
 
+        public Transform forwardDownGroundRoot;
+        public float forwardDownGroundDistance = 2.0f;
+        public bool forwardDownGroundIsHit;
+        private RaycastHit forwardDownGroundHit;
+
+        private Vector3 climbCenter;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -32,11 +40,22 @@ namespace GPL
         // Update is called once per frame
         void FixedUpdate()
         {
-            forwardUpIsHit = Physics.Raycast(forwardUpCheckRoot.position, forwardUpCheckRoot.forward, out forwardUpHit,forwardUpCheckDistance, layerMask);
+            forwardUpIsHit = Physics.Raycast(forwardUpCheckRoot.position, forwardUpCheckRoot.forward, out forwardUpHit,forwardUpCheckDistance, obstacleLayerMask);
 
-            forwardMiddleIsHit = Physics.Raycast(forwardMiddleCheckRoot.position, forwardMiddleCheckRoot.forward, out forwardMiddleHit, forwardMiddleCheckDistance, layerMask);
+            forwardMiddleIsHit = Physics.Raycast(forwardMiddleCheckRoot.position, forwardMiddleCheckRoot.forward, out forwardMiddleHit, forwardMiddleCheckDistance, obstacleLayerMask);
 
-            forwardDownIsHit = Physics.Raycast(forwardDownCheckRoot.position, forwardDownCheckRoot.forward, out forwardDownHit, forwardDownCheckDistance, layerMask);
+            forwardDownIsHit = Physics.Raycast(forwardDownCheckRoot.position, forwardDownCheckRoot.forward, out forwardDownHit, forwardDownCheckDistance, obstacleLayerMask);
+
+            forwardDownGroundIsHit = Physics.Raycast(forwardDownGroundRoot.position, -forwardDownGroundRoot.up, out forwardDownGroundHit, forwardDownGroundDistance, groundLayerMask);
+
+            if (!forwardUpIsHit && !forwardMiddleIsHit && forwardDownIsHit)
+            {
+                climbCenter = forwardMiddleCheckRoot.position + forwardMiddleCheckRoot.forward * forwardMiddleCheckDistance * 0.5f;
+            }
+            if (!forwardUpIsHit && forwardMiddleIsHit && forwardDownIsHit)
+            {
+                climbCenter = forwardUpCheckRoot.position + forwardUpCheckRoot.forward * forwardUpCheckDistance * 0.5f;
+            }
         }
 
         private void OnDrawGizmosSelected()
@@ -44,32 +63,51 @@ namespace GPL
             if (forwardUpIsHit)
             {
                 Gizmos.color = new Color(1f, 0f, 0f, 1.0f);
+                Gizmos.DrawLine(forwardUpCheckRoot.position, forwardUpHit.point);
             }
             else
             {
                 Gizmos.color = new Color(0f, 0f, 1f, 1.0f);
+                Gizmos.DrawLine(forwardUpCheckRoot.position, forwardUpCheckRoot.position + forwardUpCheckRoot.forward * forwardUpCheckDistance);
             }
-            Gizmos.DrawLine(forwardUpCheckRoot.position, forwardUpCheckRoot.position + forwardUpCheckRoot.forward* forwardUpCheckDistance);
 
             if (forwardMiddleIsHit)
             {
                 Gizmos.color = new Color(1f, 0f, 0f, 1.0f);
+                Gizmos.DrawLine(forwardMiddleCheckRoot.position, forwardMiddleHit.point);
             }
             else
             {
                 Gizmos.color = new Color(0f, 0f, 1f, 1.0f);
+                Gizmos.DrawLine(forwardMiddleCheckRoot.position, forwardMiddleCheckRoot.position + forwardMiddleCheckRoot.forward * forwardMiddleCheckDistance);
             }
-            Gizmos.DrawLine(forwardMiddleCheckRoot.position, forwardMiddleCheckRoot.position + forwardMiddleCheckRoot.forward * forwardMiddleCheckDistance);
 
             if (forwardDownIsHit)
             {
                 Gizmos.color = new Color(1f, 0f, 0f, 1.0f);
+                Gizmos.DrawLine(forwardDownCheckRoot.position, forwardDownHit.point);
             }
             else
             {
                 Gizmos.color = new Color(0f, 0f, 1f, 1.0f);
+                Gizmos.DrawLine(forwardDownCheckRoot.position, forwardDownCheckRoot.position + forwardDownCheckRoot.forward * forwardDownCheckDistance);
             }
-            Gizmos.DrawLine(forwardDownCheckRoot.position, forwardDownCheckRoot.position + forwardDownCheckRoot.forward * forwardDownCheckDistance);
+
+            if (forwardDownGroundIsHit)
+            {
+                Gizmos.color = new Color(1f, 0f, 0f, 1.0f);
+                Gizmos.DrawLine(forwardDownGroundRoot.position, forwardDownGroundHit.point);
+            }
+            else
+            {
+                Gizmos.color = new Color(0f, 0f, 1f, 1.0f);
+                Gizmos.DrawLine(forwardDownGroundRoot.position, forwardDownGroundRoot.position + -forwardDownGroundRoot.up * forwardDownGroundDistance);
+            }
+
+
+            Gizmos.color = new Color(0f, 0f, 1f, 1.0f);
+            Gizmos.DrawLine(transform.position,climbCenter);
+            Gizmos.DrawLine(climbCenter, forwardDownGroundHit.point);
         }
     }
 }
