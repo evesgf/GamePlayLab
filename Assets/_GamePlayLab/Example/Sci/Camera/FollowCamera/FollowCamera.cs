df2a4f5f4c7939cf6c0ed536026a4976f893889a
evesgf <evesgf@163.com>
2019-09-18 00:54:33 +0800

    添加转弯半径测试和镜头偏转测试

---
+++ Assets/_GamePlayLab/Example/Sci/Camera/FollowCamera/FollowCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public float rotateAngle = 15f;
+    public float angle;
+    public float angleSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float a;
+    private void LateUpdate()
+    {
+        angle = Input.GetAxis("Horizontal");
+        a = Mathf.Lerp(a, angle, rotateAngle*Time.deltaTime);
+        transform.rotation = Quaternion.AngleAxis(a * -rotateAngle, transform.forward) * transform.rotation;
+    }
+}
---
+++ Assets/_GamePlayLab/Example/Sci/Camera/FollowCamera/MovePlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public FollowCamera cam;
+
+    public Transform avatar;
+
+    public float moveSpeed = 3f;
+    public float rotateSpeed = 5f;
+
+    private Vector3 moveDirection;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3
+        {
+            x = Input.GetAxis("Horizontal"),
+            y = 0f,
+            z = Input.GetAxis("Vertical")
+        };
+    }
+
+    float sc, sp,timer;
+    private void FixedUpdate()
+    {
+        sc = Mathf.Min(1f, moveDirection.sqrMagnitude);
+        sp = moveSpeed * Time.fixedDeltaTime;
+        if (sc < 0.1f)
+        {
+            timer += Time.fixedDeltaTime;
+            if (timer > 0.3f) sp = 0;
+        }
+        else
+        {
+            timer = 0;
+        }
+
+        _rigidbody.MovePosition(transform.position + transform.forward* sp);
+
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion q = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.fixedDeltaTime * rotateSpeed);
+
+            cam.angle = moveDirection.x;
+        }
+    }
+}
